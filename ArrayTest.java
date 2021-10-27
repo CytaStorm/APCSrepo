541f5e8173a3f400e8250e684ea9ad7864235f42
Jeff Chen <cytastorm@gmail.com>
2021-10-27 10:35:09 -0400

    delete

--- ArrayTest.java
+++ ArrayTest.java
@@ -2,6 +2,7 @@ public class ArrayTest{
    public static void main(String[]args){
         int[] anArray = new int[11];
         int arrayVar = 100;
+        String part2 = "part 2:";
         //Write the loop to initialize it
         for (int i = 0; i < anArray.length; i ++) {
           anArray[i] = arrayVar;
